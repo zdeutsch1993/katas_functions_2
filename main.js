89e2e0a2238d8711bf4d93e69e7f56e7fa77dd92
zdeutsch1993 <zdeutsch1993@gmail.com>
2020-05-26 18:56:45 -0400

    stuff

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
-function add (x, y){
+function add(x, y){
     return x + y
 }
 console.log(add(2, 4))
 
 function multiply (a, b){
+    let result = 0
     for (let i = 0; i < b; i++){
-        
+    result = add(a,result);
     }
+    return result
 }
 console.log(multiply(4, 6))
 
 function power (x, n){
     let coke = 1;
     for (let i = 0; i < n; i++){
-    coke *= x;
+    coke = multiply(x, coke);
     }
     return coke
 }
 console.log(power(2,8))
+
+function factorial(f){
+    
+}
